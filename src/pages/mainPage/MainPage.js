3dacb56e1a7d6c3272e59137c1643fefb6d177eb
mrsBarul <dobro.ve@mail.ru>
2024-09-13 18:32:10 +0300

    fix mistake

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -5,7 +5,7 @@ import SliderChat from '../../entities/sliderChat/ui/sliderChat/SliderChat';
 import SliderToolkit from '../../entities/sliderToolkit/ui/sliderToolkit/SliderToolkit';
 import useWindowSize from '../../shared/hooks/useWindowSize';
 import Stack from '../../shared/ui/stack/Stack';
-import Footer from '../../widgets/ui/footer/ui/footer/Footer';
+// import Footer from '../../widgets/ui/footer/ui/footer/Footer';
 import Header from '../../widgets/ui/header/mainHeader/Header';
 import HeaderTopMenu from '../../widgets/ui/header/menuTop/ui/headerTopMenu/HeaderTopMenu';
 import Navbar from '../../widgets/ui/navbar/ui/navbar/Navbar';
@@ -28,7 +28,7 @@ const MainPage = () => {
                 <SliderToolkit/> 
                 <Poster/>
                 <Questions/>
-                <Footer/> 
+                {/* <Footer/>  */}
             </Stack>
         </main>
     )
